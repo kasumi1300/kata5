f4969f66ba9ceb19117dad0352dfce83dc6f775f
usuario <usuario@10.22.146.207>
2013-11-15 11:57:17 +0000

    First commit

--- src/kata5/Kata5.java
+++ src/kata5/Kata5.java
@@ -7,9 +7,15 @@ public class Kata5 {
     
     public static void main(String[] args) {
         MockMailLoader loader = new MockMailLoader();
-        HistogramBuilder<Mail> builder = new HistogramBuilder<>();
-        builder.build(loader.load());
-        HistogramViewer<Mail> viewer = new HistogramViewer<>();
-        viewer.show(builder.getHistogram());
+        HistogramBuilder<Mail> builder = new HistogramBuilder<>(loader.load());
+        Histogram<String> histogram = builder.build(new AttributeExtractor<Mail, String>(){
+            @Override
+            public String extract (Mail entity){
+                return entity.getDomain();          
+            }
+        });
+        HistogramViewer<String> viewer = new HistogramViewer<>();
+        viewer.show(histogram);
+        
     }
 }
